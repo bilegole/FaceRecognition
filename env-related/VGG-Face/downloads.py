0108f366dd65016b2e29ed276705c6bb90d80c11
yuang <958102068@qq.com>
2020-12-28 11:23:33 +0800

    cmd auto update

--- env-related/VGG-Face/downloads.py
+++ env-related/VGG-Face/downloads.py
@@ -1,32 +1,32 @@
+import requests
+import os
+
 # author：yuang
 # description：
 #   主要目的：下载VGG数据集。
 #   下载网站：https://www.robots.ox.ac.uk/~vgg/software/vgg_face/
 #   下载地址：
-downloads_links={
-    "torch.tar.gz":"https://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_torch.tar.gz",
-    "caffe.tar.gz":"https://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_caffe.tar.gz",
-    "matconvnet.tar.gz":"https://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_matconvnet.tar.gz"
+downloads_links = {
+    "torch.tar.gz": "https://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_torch.tar.gz",
+    "caffe.tar.gz": "https://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_caffe.tar.gz",
+    "matconvnet.tar.gz": "https://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_matconvnet.tar.gz"
 }
 
 #   下载到以下文件夹：
-target="../.cache/"
+target = "../.cache/"
 #
 
 # 下载执行：
-import requests
-import os
+
 
 print(f"准备下载文件\n目标文件夹：{os.path.abspath(target)}\n")
 for name in downloads_links:
-    rfile = requests.get(downloads_links[name],stream=True)
-    download_size=0
-    chuck_size=512
-    with open(os.path.join(target,name),'wb') as file:
-        for chunk in rfile.iter_content(chunk_size=chunk_size):
-            if chuck:
-                f.write(chunk)
-                download_size+=512
-
-
-
+    print(f"正在下载文件：{name}")
+    remote_file = requests.get(downloads_links[name], stream=True)
+    download_size = 0
+    chuck_size = 512
+    with open(os.path.join(target, name), 'wb') as file:
+        for chunk in remote_file.iter_content(chunk_size=chuck_size):
+            if chunk:
+                file.write(chunk)
+                download_size += 512
